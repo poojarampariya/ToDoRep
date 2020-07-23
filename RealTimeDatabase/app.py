8bea4af7e864ab959eb3f6b1b55f9112f9f1cfd1
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-23 15:14:21 +0530

    Update app.py

--- RealTimeDatabase/app.py
+++ RealTimeDatabase/app.py
@@ -1,13 +1,13 @@
 import pyrebase
 config = {
-    apiKey: "AIzaSyDsRWUKzPSNBDWWScXH0nL0n3Sn_CD20xc",
-    authDomain: "fir-project-f8aa4.firebaseapp.com",
-    databaseURL: "https://fir-project-f8aa4.firebaseio.com",
-    projectId: "fir-project-f8aa4",
-    storageBucket: "fir-project-f8aa4.appspot.com",
-    messagingSenderId: "582440646450",
-    appId: "1:582440646450:web:6f529e373403e8d165092b",
-    measurementId: "G-TE8BGX11BY"
+    "apiKey": "AIzaSyDsRWUKzPSNBDWWScXH0nL0n3Sn_CD20xc",
+    "authDomain": "fir-project-f8aa4.firebaseapp.com",
+    "databaseURL": "https://fir-project-f8aa4.firebaseio.com",
+    "projectId": "fir-project-f8aa4",
+    "storageBucket": "fir-project-f8aa4.appspot.com",
+    "messagingSenderId": "582440646450",
+    "appId": "1:582440646450:web:6f529e373403e8d165092b",
+    "measurementId": "G-TE8BGX11BY"
   };
 
 firebase = pyrebase.initialize_app(config)
@@ -34,6 +34,6 @@ def basic():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
+   
+    app.run(host='127.0.0.1', port=8080, debug=True)
 
